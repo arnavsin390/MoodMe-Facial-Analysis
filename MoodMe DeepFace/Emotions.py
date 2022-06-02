1f8f34d43a4cff96c01d038d65b7d97a01d50fed
arnavsin390 <77707535+arnavsin390@users.noreply.github.com>
2022-06-02 13:58:33 -0700

    Final Website
    Added Error Handling and Storage Optimization

--- MoodMe DeepFace/Emotions.py
+++ MoodMe DeepFace/Emotions.py
@@ -6,56 +6,56 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-#app.config['\\UPLOADS']
-
-#path = "face_imag.png"
-#image = cv2.imread(path)
-
-#analyze = DeepFace.analyze(image, actions=['emotion'])
-#print(analyze)
-#print(analyze['dominant_emotion'])
-
 path = "None"
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "POST":
-        global path
-        picture = request.files["pic"]
-        path = secure_filename(picture.filename)
-        #path = "global"
-        picture.save(path)
-        if path == "None":
-            return render_template("site.html")
-            print("Path", path)
-        else:
-            return redirect(url_for("results"))
-
-        #print(analyze)
-        #print(analyze['dominant_emotion'])`z
+    try:
+        if request.method == "POST":
+            global path
+            picture = request.files["pic"]
+            path = secure_filename(picture.filename)
+            picture.save(path)
+            if path == "None":
+                return render_template("site.html")
+            else:
+                return redirect(url_for("results"))
+    except:
+        return render_template("site.html", error="Please enter a valid file type with a face")
     else:
         return render_template("site.html")
 
+
 @app.route("/results", methods=["POST", "GET"])
 def results():
-    global path
-    image = cv2.imread(path)
-    print(path)
-    analyze = DeepFace.analyze(path, actions=['emotion'])
-    dominant = analyze['dominant_emotion']
-    #print(analyze)
-    #print(dominant)
-    for item in analyze['emotion']:
-        round(analyze['emotion'][item], 2)
-    return render_template("results.html", path=path, pathtype = type(path), dominant=dominant,
-                           emotions=analyze['emotion'])
+    try:
+        global path
+        analyze = DeepFace.analyze(path, actions=['emotion'])
+        dominant = analyze['dominant_emotion']
+        for item in analyze['emotion']:
+            round(analyze['emotion'][item], 2)
+        remove_file()
+        return render_template("results.html", path=path, dominant=dominant,
+                               emotions=analyze['emotion'])
+    except:
+        remove_file()
+        return render_template("site.html", error="Please enter a valid file type with a face")
+
+
+def remove_file():
+    defaults = ["Emotions.py", "global", "templates"]
+    for filename in os.listdir():
+        if filename not in defaults:
+            print("remove", filename)
+            os.remove(filename)
+
 
 @app.route('/<filename>')
 def upload(filename):
     return send_from_directory("/", filename)
 
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True)
-
